6bc0bb3a38aff96419e0b846b2bed55d11c5c317
Tony <effrenanthony2704@gmail.com>
2023-01-12 00:16:43 -0500

    useLocalStorage working and explained

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -3,50 +3,94 @@ import defaultTasks from '../../mocks/tasks';
 import { TaskListView, ModalComponent, CreateTaskForm } from '../../components';
 import useLocalStorage from '../../hooks/useLocalStorage';
 
+// function Fantasma() {
+//   useEffect(() => {
+//     console.log('apareci');
+//     return () => {
+//       console.log('me fui');
+//     };
+//   }, []);
+//   return <div>Soy el fantasma</div>;
+// }
 
 function HomeComponent() {
-  const [taskList, setTaskList] = useState<TSTask[]>([]);
+  // const [pokemon, setPokemon] = useState<any>('');
+  // const [pokeId, setPokeId] = useState<any>(1);
+  // const [mostrar, setMostrar] = useState<any>(false);
+  // const [taskList, setTaskList] = useState<TSTask[]>([]);
   const [openModal, setOpenModal] = useState<boolean>(false);
-  // YA QUE USAMOS UN USE EFFECT EN USELOCALSTORAGE, 
-  // ESTE TIENE QUE ESTAR A LA ALTURA DEL COMPONENTE
-  // QUE RENDERIZA LOS TASK, SINO NO VA A ACTUALIZARLOS
-  // PORQUE EL RENDER SERA SOLO DEL COMPONENTE DONDE SE USÓ EL HOOK
-  const { item, saveItem } = useLocalStorage('taskList', [])
+  // ! YA QUE USAMOS UN USE EFFECT EN USELOCALSTORAGE,
+  // ! ESTE TIENE QUE ESTAR A LA ALTURA DEL COMPONENTE
+  // ! QUE RENDERIZA LOS TASK, SINO NO VA A ACTUALIZARLOS
+  // ! PORQUE EL RENDER SERA SOLO DEL COMPONENTE DONDE SE USÓ EL HOOK
+  const { item, saveItem } = useLocalStorage('taskList', []);
   // const [number, setNumber] = useState<number>(0);
   // const [show, setShow] = useState<boolean>(false);
   // console.log('render');
   // let number = 0;
   // fetch
   // useEffect(() => {
-  //   setTimeout(() => {
-  //     setTaskList(defaultTasks);
-  //   }, Math.floor(Math.random() * 5000) + 1000);
-  // }, [taskList]);
+  //   // setTimeout(() => {
+  //   //   setTaskList(defaultTasks);
+  //   //   console.log('render')
+  //   // }, Math.floor(Math.random() * 5000) + 1000);
+  //   async function getPokemon() {
+  //     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
+  //     const data = await res.json();
+  //     setPokemon(data);
+  //   }
+  //   getPokemon();
+  // }, [
+  //   // taskList
+  //   // !!! pokemon
+  //   pokeId,
+  // ]);
 
   const handleOpenModal = () => {
-    setOpenModal(!openModal)
-  }
+    setOpenModal(!openModal);
+  };
   return (
     <div>
       <h1>Task Manager</h1>
       <div>
-        <button type="button"
-          onClick={handleOpenModal}
-        >Nueva Tarea</button>
+        <button type="button" onClick={handleOpenModal}>
+          Nueva Tarea
+        </button>
       </div>
+      {/* <div>
+        <button
+          type="button"
+          onClick={() => {
+            setPokeId(pokeId + 1);
+          }}
+        >
+          Otro Pokemon
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setMostrar(!mostrar);
+          }}
+        >
+          {mostrar ? 'desaparecer' : 'aparecer'}
+        </button>
+      </div>
+      {pokemon.name}
+      {mostrar && <Fantasma />} */}
       <TaskListView taskList={item! as TSTask[]} />
       <div />
-      <ModalComponent 
+      <ModalComponent
         modalIsOpen={openModal}
         closeModal={handleOpenModal}
         title="New Task"
       >
         <CreateTaskForm
-        saveItem={saveItem}
-        item={item}
-        closeModal={handleOpenModal}
+          saveItem={saveItem}
+          item={item}
+          closeModal={handleOpenModal}
         />
-        
       </ModalComponent>
     </div>
   );
--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,38 +1,38 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 
-function useLocalStorage(itemName:string, initialValue:any) {
-  const [item, setItem] = useState(initialValue)
+function useLocalStorage(itemName: string, initialValue: any) {
+  const [item, setItem] = useState(initialValue);
   // const [sincronizeItem, setSincronizeItem] = useState(true);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | undefined>()
+  const [error, setError] = useState<string | undefined>();
 
   useEffect(() => {
+    setLoading(true);
     setTimeout(() => {
-      const localStorageItem = window.localStorage.getItem(itemName)
-      if(localStorageItem) {
-        setItem(JSON.parse(localStorageItem))
+      const localStorageItem = window.localStorage.getItem(itemName);
+      if (localStorageItem) {
+        setItem(JSON.parse(localStorageItem));
       } else {
-        setItem(initialValue)
-        setError('Item not found')
+        setItem(initialValue);
+        setError('Item not found');
       }
-      setLoading(false)
+      setLoading(false);
       // setSincronizeItem(true)
-    // }, Math.floor(Math.random() * 5000) + 1000);
-    }, 1000);
-  }, [])
+      // }, Math.floor(Math.random() * 5000) + 1000);
+    }, Math.floor(Math.random() * 5000) + 1000);
+  }, []);
 
-  const saveItem = (newItem:any) => {
-    window.localStorage.setItem(itemName, JSON.stringify(newItem))
-    setItem(newItem)
-  }
+  const saveItem = (newItem: any) => {
+    window.localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  };
 
   return {
     item,
     error,
     saveItem,
-    loading
-  }
-
+    loading,
+  };
 }
 
-export default useLocalStorage
+export default useLocalStorage;
